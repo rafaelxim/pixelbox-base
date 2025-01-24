b44c71e82d6d4b1f98380820bac85454ca094de5
Rafael Herculano <rafa.lj@hotmail.com>
2025-01-24 14:54:19 -0300

    change colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
