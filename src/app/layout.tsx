127e34eeeb8242f141495de8f8243b6bf56bcd29
Rafael Herculano <rafa.lj@hotmail.com>
2025-01-25 16:01:24 -0300

    fix subset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.scss";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
