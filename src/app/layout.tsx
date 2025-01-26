078af023a290937b1b65fb15ff8f2fab7d99ad56
Rafael Herculano <rafa.lj@hotmail.com>
2025-01-26 11:55:37 -0300

    add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,16 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-3KLCQJ28VL"
-      />
-      <Script id="gtag">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-3KLCQJ28VL');`}
+      <Script id="tagmanager">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTT277H4');`}
       </Script>
       <body className={poppins.className}>{children}</body>
     </html>
