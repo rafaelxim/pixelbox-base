fbdad0c73013bcb53a695c164ae0d8df1bb5572d
Rafael Herculano <rafa.lj@hotmail.com>
2025-01-28 11:32:00 -0300

    rota de landing pages

---
+++ src/app/landing-page/data.ts
@@ -0,0 +1,189 @@
+const DATA = {
+  header: {
+    title: "Criação de <span>Landing Page</span>",
+    description:
+      "Criamos sua Landing Page e fazemos dela uma MÁQUINA de aquisição de clientes!  Landing Pages que irão aumentar sua taxa de conversão e suas suas chances de fechar novos negócios. Tudo isso com um excelente custo-benefício e entregue em 5 dias.",
+    mainImage: "/images/sites-examples.png",
+  },
+  pains: {
+    title: "Você perde oportunidades todos os dias...",
+    items: [
+      "Sua página não convence e acaba espantando clientes.",
+      "Seus anúncios não convertem porque falta uma página profissional",
+      "Você perde vendas porque ninguém sabe o que você oferece",
+      "Sua marca parece amadora ou parada no tempo",
+      "Não aparece bem no Google e fica atrás da concorrência",
+      "Clientes visitam sua página, mas vão embora sem fazer nada",
+    ],
+  },
+  benefits: {
+    subTitle: "Imagine sua landing page transformando visitantes em clientes",
+    title: "Com certeza ela teria...",
+    items: [
+      "+ Mais conversões",
+      "+ Leads qualificados(clientes)",
+      "+ Resultados rápidos",
+    ],
+    description:
+      "Criamos landing pages focadas em conversão, utilizando um método exclusivo que maximiza a captação de leads e gera resultados imediatos para o seu negócio.",
+    image: "/images/smartphone-neon.avif",
+  },
+  portfolio: {
+    subtitle: "Projetos que Transformaram Negócios",
+    title: "Alguns trabalhos desenvolvidos",
+    items: [
+      "/images/site1.png",
+      "/images/site2.png",
+      "/images/site3.png",
+      "/images/site4.png",
+      "/images/site5.png",
+      "/images/site6.png",
+      "/images/site7.png",
+      "/images/site8.png",
+    ],
+  },
+
+  advantages: {
+    title: "soluções de marketing comum",
+    items1: [
+      "Atendimento próximo e humanizado",
+      "Conhecimento profundo sobre cada cliente",
+      "Método exclusivo e comprovado",
+      "Especialistas em desenvolvimento",
+    ],
+    items2: [
+      "Resultados superiores",
+      "Suporte de uma equipe parceira e sempre disponível",
+    ],
+    subtitle: "Não somos uma empresa de",
+    subtitle1:
+      "Os diferenciais que estão fazendo nossa empresa ser um grande sucesso são:",
+    subtitle2:
+      "Quem nos contrata afirma que vale a pena porque isso proporciona",
+    image: "/images/team.png",
+  },
+
+  testimonials: {
+    subtitle: "Valorizamos",
+    title: "nossos clientes",
+    text1: "A satisfação dos nossos clientes é a nossa maior conquista.",
+    text2:
+      "Por isso, valorizamos cada feedback que recebemos e estamos sempre buscando melhorar nosso atendimento e serviços.",
+  },
+  strategy: {
+    subtitle: "Só uma landing page não basta",
+    title: "É preciso estratégia para converter mais",
+    text1:
+      "Para ter uma Landing Page de Alta conversão você precisa contar com textos persuasivos, CTA bem definido, página responsiva com carregamento rápido, além de várias técnicas de SEO",
+    text2: "Fique tranquilo, cuidamos disso tudo, você só curte o resultado!",
+  },
+  pillars: {
+    subtitle: "Atuamos",
+    title: "em 3 pilares",
+    cards: [
+      {
+        title: "Criação de Landing Pages",
+        image: "/icons/website.svg",
+        description:
+          "Desenvolvemos landing pages personalizadas e totalmente configuradas. Páginas estratégicas, otimizadas para SEO e com foco em conversão, prontas para captar leads e gerar resultados.",
+      },
+      {
+        title: "Estratégia Digital",
+        image: "/icons/strategy.svg",
+        description:
+          "Tudo começa com uma boa estratégia. Criamos páginas alinhadas à jornada do cliente, garantindo um marketing eficiente e resultados consistentes.",
+      },
+      {
+        title: "Tráfego Pago e Orgânico",
+        image: "/icons/strategy.svg",
+        description:
+          "Impulsionamos suas landing pages com Google Ads e estratégias orgânicas, adaptadas para atrair o público certo e gerar mais conversões.",
+      },
+    ],
+  },
+  facilities: {
+    subtitle: "Landing Pages de alto desempenho",
+    title: "e personalizadas em 5 dias",
+    cards: [
+      {
+        title: "Página Responsiva",
+        description:
+          "Desenvolvemos landing pages que se ajustam perfeitamente a computadores, tablets e smartphones, oferecendo uma experiência otimizada para seus visitantes.",
+        image: "/icons/responsivo.svg",
+      },
+      {
+        title: "SEO otimizado",
+        description:
+          "Criamos landing pages com técnicas de SEO avançadas para garantir que sua página apareça nos primeiros resultados dos motores de busca.",
+        image: "/icons/google.svg",
+      },
+      {
+        title: "Layout sob medida",
+        description:
+          "Desenhamos a landing page com base na identidade visual da sua marca, criando um design atrativo e eficaz para conversões.",
+        image: "/icons/layout.svg",
+      },
+      {
+        title: "Atendimento próximo",
+        description:
+          "Oferecemos suporte dedicado via WhatsApp ou videochamada para tirar dúvidas e atender a todas as suas necessidades durante o processo.",
+        image: "/icons/atendimento.svg",
+      },
+      {
+        title: "Aumento de conversões",
+        description:
+          "Utilizamos estratégias para aumentar as conversões da sua landing page, integrando-a às principais ferramentas de marketing digital.",
+        image: "/icons/impulsao.svg",
+      },
+      {
+        title: "Manutenção contínua",
+        description:
+          "Mantemos sua landing page sempre otimizada e funcionando perfeitamente, oferecendo suporte técnico para ajustes e melhorias quando necessário.",
+        image: "/icons/manutencao.svg",
+      },
+    ],
+  },
+
+  faq: {
+    questions: [
+      {
+        id: 1,
+        question: "Minha landing page estará no Google?",
+        answer:
+          "Sim! Incluímos no nosso trabalho de desenvolvimento a otimização da sua landing page para SEO, garantindo que ela tenha visibilidade nos motores de busca e atraia tráfego qualificado.",
+      },
+      {
+        id: 2,
+        question: "Qual o prazo de entrega da landing page?",
+        answer:
+          "Após a contratação, faremos um briefing para entender suas necessidades e a identidade visual da sua marca. Com todas as informações coletadas, entregamos a landing page em até 5 dias úteis.",
+      },
+      {
+        id: 3,
+        question: "Vocês registram o domínio da landing page?",
+        answer:
+          "Sim, ao nos contratar, fazemos toda a configuração da sua landing page, incluindo o registro e configuração do seu domínio (ex: www.pagina.com.br), além de garantir que tudo esteja otimizado para o seu sucesso.",
+      },
+      {
+        id: 4,
+        question: "Quero fazer um orçamento. Como faço?",
+        answer:
+          "Nossa equipe de consultores está pronta para ajudá-lo a entender suas necessidades e criar soluções personalizadas. Entre em contato conosco pelo WhatsApp e descubra como podemos ajudar a transformar seus objetivos em realidade. O orçamento é totalmente gratuito.",
+      },
+      {
+        id: 5,
+        question: "Vocês oferecem suporte técnico após a conclusão do projeto?",
+        answer:
+          "Sim, oferecemos suporte técnico contínuo e humanizado via WhatsApp, garantindo que sua landing page funcione perfeitamente e tirando dúvidas sobre sua operação após o lançamento.",
+      },
+      {
+        id: 6,
+        question: "Como a PixelBox pode ajudar no meu negócio?",
+        answer:
+          "Nossa equipe pode ajudá-lo a criar landing pages otimizadas para conversões, atração de leads e aumento de vendas. Converse conosco e descubra como podemos potencializar sua presença online.",
+      },
+    ],
+  },
+};
+
+export default DATA;
---
+++ src/app/landing-page/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Advantages from "../sections/Advantages";
+import Benefits from "../sections/Benefits";
+import Facilities from "../sections/Facilities";
+import Footer from "../sections/Footer";
+import Form from "../sections/Form";
+import FrequentQuestions from "../sections/FrequentQuestions";
+import Header from "../sections/Header";
+import Pains from "../sections/Pains";
+import Pillars from "../sections/Pillars";
+import Portfolio from "../sections/Portfolio";
+import Strategy from "../sections/Strategy";
+import Testimonials from "../sections/Testimonials";
+import DATA from "./data";
+
+const Home = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div>
+      <Header {...DATA.header} />
+      <Pains {...DATA.pains} />
+      <Benefits {...DATA.benefits} />
+      <Portfolio {...DATA.portfolio} />
+      <Advantages {...DATA.advantages} />
+      <Testimonials {...DATA.testimonials} />
+      <Strategy {...DATA.strategy} />
+      <Pillars {...DATA.pillars} />
+      <Facilities {...DATA.facilities} />
+      <FrequentQuestions {...DATA.faq} />
+      <Form />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
