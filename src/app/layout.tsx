f07a1ef6e1b03176a19dbd7b30750b22b8c1e471
Rafael Herculano <rafa.lj@hotmail.com>
2025-01-30 09:32:45 -0300

    execucao do tag manager apenas em prd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  console.log({ isProduction });
+
   return (
     <html lang="pt-br">
-      <Script id="tagmanager">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KTT277H4');`}
-      </Script>
+      {isProduction && (
+        <Script id="tagmanager">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer','GTM-KTT277H4');`}
+        </Script>
+      )}
+
       <body className={poppins.className}>{children}</body>
     </html>
   );
