38329651abddbf64e29ba6ca89d14179e428fdb9
Rafael Herculano <rafa.lj@hotmail.com>
2025-10-26 15:58:07 -0300

    corrige importacao pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,7 @@ export default function RootLayout({
 
       {isProduction && (
         <Script id="pixelMeta">
-          {`<script>
-!function(f,b,e,v,n,t,s)
+          {`!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -48,8 +47,7 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '1459820958423868');
 fbq('track', 'PageView');
-</script>`}
-        </Script>
+`}</Script>
       )}
 
       <body className={poppins.className}>
